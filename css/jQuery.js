9d26067951dd4ef6b2507b7da854cbc30607e4d6
Solene-G <Solene-G@users.noreply.github.com>
2021-01-21 09:53:14 +0100

    Portfolio

---
+++ css/jQuery.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+			jQuery(".bar-color").css({
+				"background": "#2196F3",
+				"width": "95%"
+			});
+			jQuery(".skills:nth-of-type(2) .bar-color").css({
+				"width": "80%"
+			});
+			jQuery(".skills:nth-of-type(3) .bar-color").css({
+				"width": "70%"
+			});
+			jQuery(".skills:nth-of-type(4) .bar-color").css({
+				"width": "55%"
+			});
+		});
